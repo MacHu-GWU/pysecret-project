963563908c548b837cb8c20a1f8db3a89e2d2ada
Sanhe Hu <husanhe@gmail.com>
2023-02-10 17:45:23 -0500

    refactor parameter store

---
+++ pysecret/aws/parameter_store.py
@@ -0,0 +1,368 @@
+# -*- coding: utf-8 -*-
+
+import typing as T
+import json
+import enum
+import dataclasses
+from datetime import datetime
+
+try:
+    import jsonpickle
+
+    has_jsonpickle = True
+except ImportError:  # pragma: no cover
+    has_jsonpickle = False
+
+from ..compat import cached_property
+from ..js_helper import strip_comments
+from ..helper import ensure_only_one_true
+
+JSON_PICKLE_KEY = "__jsonpickle__"
+
+
+class ParameterTypeEnum(str, enum.Enum):
+    string = "String"
+    string_list = "StringList"
+    secure_string = "SecureString"
+
+
+DEFAULT_KMS_KEY = "alias/aws/ssm"
+
+
+class ParameterTierEnum(str, enum.Enum):
+    standard = "Standard"
+    advanced = "Advanced"
+    intelligent_tiering = "Intelligent-Tiering"
+
+
+class ParameterDataTypeEnum(str, enum.Enum):
+    text = "text"
+    ec2_image = "aws:ec2:image"
+
+
+@dataclasses.dataclass
+class Parameter:
+    Name: str = dataclasses.field()
+    Type: str = dataclasses.field()
+    Value: str = dataclasses.field()
+    Version: int = dataclasses.field()
+    LastModifiedDate: datetime = dataclasses.field()
+    DataType: str = dataclasses.field()
+    ARN: T.Optional[str] = dataclasses.field(default=None)
+    Selector: T.Optional[str] = dataclasses.field(default=None)
+    SourceResult: T.Optional[str] = dataclasses.field(default=None)
+
+    @classmethod
+    def load(
+        cls,
+        ssm_client,
+        name: str,
+        with_decryption: T.Optional[bool] = None,
+    ) -> T.Optional["Parameter"]:
+        """
+        Ref:
+
+        - get_parameter: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm.html#SSM.Client.get_parameter
+        """
+        kwargs = dict(Name=name)
+        if with_decryption is not None:
+            kwargs["WithDecryption"] = with_decryption
+
+        # get the parameter data
+        try:
+            response = ssm_client.get_parameter(**kwargs)
+            parameter = cls(
+                Name=response["Parameter"]["Name"],
+                Type=response["Parameter"]["Type"],
+                Value=response["Parameter"]["Value"],
+                Version=response["Parameter"]["Version"],
+                LastModifiedDate=response["Parameter"]["LastModifiedDate"],
+                DataType=response["Parameter"]["DataType"],
+                ARN=response["Parameter"]["ARN"],
+                Selector=response["Parameter"].get("Selector"),
+                SourceResult=response["Parameter"].get("SourceResult"),
+            )
+            # check if the Type is secure string
+            if parameter.Type == ParameterTypeEnum.secure_string.value:
+                # if already set with_decryption = True, then return
+                if with_decryption is True:
+                    return parameter
+                # if forget to set with_description = True, then do it again
+                else:
+                    return cls.load(ssm_client, name, True)
+            else:
+                return parameter
+        # if not exists, return None
+        except Exception as e:
+            if "ParameterNotFound" in str(e):
+                return None
+            else:  # pragma: no cover
+                raise e
+
+    @classmethod
+    def _from_put_parameter_response(
+        cls,
+        put_parameter_kwargs: dict,
+        put_parameter_response: dict,
+    ):
+        return Parameter(
+            Name=put_parameter_kwargs["Name"],
+            Type=put_parameter_kwargs["Type"],
+            Value=put_parameter_kwargs["Value"],
+            Version=put_parameter_response["Version"],
+            LastModifiedDate=datetime.strptime(
+                put_parameter_response["ResponseMetadata"]["HTTPHeaders"]["date"],
+                "%a, %d %b %Y %H:%M:%S %Z",
+            ),
+            ARN=None,
+            DataType="text",
+        )
+
+    @cached_property
+    def string_list(self) -> T.List[str]:
+        return self.Value.split(",")
+
+    @cached_property
+    def json_dict(self) -> dict:
+        return json.loads(strip_comments(self.Value))
+
+    @cached_property
+    def json_list(self) -> list:
+        return json.loads(strip_comments(self.Value))
+
+    @cached_property
+    def py_object(self):
+        if has_jsonpickle is False:  # pragma: no cover
+            raise ImportError(
+                "you have to install `jsonpickle` to store arbitrary "
+                "python object in AWS Parameter Store."
+            )
+        return jsonpickle.loads(json.loads(self.Value)[JSON_PICKLE_KEY])
+
+    @property
+    def aws_account_id(self) -> str:
+        return self.ARN.split(":")[4]
+
+    @property
+    def aws_region(self) -> str:
+        return self.ARN.split(":")[3]
+
+    # @cached_property
+    # def labels(self) -> dict:
+    #     return pass
+
+
+def deploy_parameter(
+    ssm_client,
+    name: str,
+    data: T.Union[str, list, dict, T.Any],
+    description: T.Optional[str] = None,
+    type_is_string: bool = False,
+    type_is_string_list: bool = False,
+    type_is_secure_string: bool = False,
+    kms_key_id: T.Optional[str] = None,
+    use_default_kms_key: T.Optional[bool] = False,
+    tier_is_standard: bool = False,
+    tier_is_advanced: bool = False,
+    tier_is_intelligent: bool = False,
+    policies: T.Optional[str] = None,
+    tags: T.Dict[str, str] = None,
+    overwrite: bool = False,
+    skip_if_duplicated: bool = True,
+) -> T.Optional[Parameter]:
+    """
+    Create or Update a parameter.
+
+    Ref:
+
+    - put_parameter: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm.html#SSM.Client.put_parameter
+
+    :param ssm_client: boto3 system manager client
+    :param name:
+    :param data:
+    :param description:
+    :param kms_key_id:
+    :param use_default_kms_key: if true, then you can omit the ``kms_key_id``
+        field, and it will use the default ``alias/aws/ssm`` kms key to encrypt
+        your data, and the type become Se
+    :param tier:
+    :param policies:
+    :param tags:
+    :param overwrite:
+    :param skip_if_duplicated: if True, then won't do deployment if parameter data
+        is the same as the one in the latest version.
+
+    :rtype: dict
+    """
+
+    # --------------------------------------------------------------------------
+    # input argument pre processing
+    # --------------------------------------------------------------------------
+    put_parameter_kwargs = {
+        "Name": name,
+        "Overwrite": overwrite,
+    }
+
+    # type
+    ensure_only_one_true(
+        [
+            ("type_is_string", type_is_string),
+            ("type_is_string_list", type_is_string_list),
+            ("type_is_secure_string", type_is_secure_string),
+        ]
+    )
+
+    if type_is_string:
+        put_parameter_kwargs["Type"] = ParameterTypeEnum.string.value
+    if type_is_string_list:
+        put_parameter_kwargs["Type"] = ParameterTypeEnum.string_list.value
+    if type_is_secure_string:
+        put_parameter_kwargs["Type"] = ParameterTypeEnum.secure_string.value
+
+    # value
+    if isinstance(data, str):
+        put_parameter_kwargs["Value"] = data
+    elif isinstance(data, list):
+        if type_is_string_list:
+            for item in data:
+                if "," in item:
+                    raise ValueError(
+                        "your parameter data has ',' in it, "
+                        "you cannot use StringList type, "
+                        "please use String type!"
+                    )
+            put_parameter_kwargs["Value"] = ",".join(data)
+        else:
+            put_parameter_kwargs["Value"] = json.dumps(data)
+    elif isinstance(data, dict):
+        put_parameter_kwargs["Value"] = json.dumps(data)
+    else:
+        if has_jsonpickle is False:  # pragma: no cover
+            raise ImportError(
+                "you have to install `jsonpickle` to store arbitrary "
+                "python object in AWS Parameter Store."
+            )
+        put_parameter_kwargs["Value"] = json.dumps(
+            {JSON_PICKLE_KEY: jsonpickle.dumps(data)}
+        )
+
+    # description
+    if description is not None:
+        put_parameter_kwargs["Description"] = description
+
+    # encryption related
+    with_encryption = False
+    if (kms_key_id is not None) and (use_default_kms_key is True):
+        raise ValueError(
+            "you cannot set `kms_key_id` and `use_default_kms_key = True` "
+            "at same time!"
+        )
+    elif (kms_key_id is not None) and (use_default_kms_key is not True):
+        if type_is_secure_string is False:
+            raise ValueError(
+                f"you cannot set kms_key_id = {kms_key_id!r}, "
+                f"use_default_kms_key = {use_default_kms_key!r} "
+                f"when type is SecureString!"
+            )
+        put_parameter_kwargs["KeyId"] = kms_key_id  # pragma: no cover
+        with_encryption = True  # pragma: no cover
+    elif (kms_key_id is None) and (use_default_kms_key is True):
+        if type_is_secure_string is False:
+            raise ValueError(
+                f"you cannot set kms_key_id = {kms_key_id!r}, "
+                f"use_default_kms_key = {use_default_kms_key!r} "
+                f"when type is SecureString!"
+            )
+        put_parameter_kwargs["KeyId"] = DEFAULT_KMS_KEY
+        with_encryption = True
+    elif (kms_key_id is None) and (use_default_kms_key is not True):
+        if type_is_secure_string is True:
+            put_parameter_kwargs["KeyId"] = DEFAULT_KMS_KEY
+            with_encryption = True
+    else:  # pragma: no cover
+        raise NotImplementedError
+
+    # tier
+    ensure_only_one_true(
+        [
+            ("tier_is_standard", tier_is_standard),
+            ("tier_is_advanced", tier_is_advanced),
+            ("tier_is_intelligent", tier_is_intelligent),
+        ]
+    )
+
+    if tier_is_standard:
+        put_parameter_kwargs["Tier"] = ParameterTierEnum.standard.value
+    if tier_is_advanced:
+        put_parameter_kwargs["Tier"] = ParameterTierEnum.advanced.value
+    if tier_is_intelligent:
+        put_parameter_kwargs["Tier"] = ParameterTierEnum.intelligent_tiering.value
+
+    # policy
+    if policies is not None:  # pragma: no cover
+        put_parameter_kwargs["Policies"] = policies
+
+    # tag
+    if tags is None:
+        tags = dict()
+    Tags = [{"Key": k, "Value": v} for k, v in tags.items()]
+    if len(Tags):
+        put_parameter_kwargs["Tags"] = Tags
+
+    # overwrite
+    if overwrite:
+        put_parameter_kwargs["Overwrite"] = overwrite
+
+    # --------------------------------------------------------------------------
+    # create or update
+    # --------------------------------------------------------------------------
+    # check duplication
+    if skip_if_duplicated:
+        # check if parameter exists
+        parameter = Parameter.load(
+            ssm_client=ssm_client,
+            name=name,
+            with_decryption=with_encryption,
+        )
+        # if not exists, do create
+        if parameter is None:
+            response = ssm_client.put_parameter(**put_parameter_kwargs)
+            return Parameter._from_put_parameter_response(
+                put_parameter_kwargs, response
+            )
+        # if already exists, compare the parameter data
+        else:
+            # if the same, do nothing
+            if parameter.Value == put_parameter_kwargs["Value"]:
+                return None
+            # if not same, do update
+            else:
+                response = ssm_client.put_parameter(**put_parameter_kwargs)
+                return Parameter._from_put_parameter_response(
+                    put_parameter_kwargs, response
+                )
+    # don't duplication check, just update
+    else:
+        response = ssm_client.put_parameter(**put_parameter_kwargs)
+        return Parameter._from_put_parameter_response(put_parameter_kwargs, response)
+
+
+def delete_parameter(
+    ssm_client,
+    name: str,
+) -> bool:
+    """
+    Ref:
+
+    - delete_parameter: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm.html#SSM.Client.delete_parameter
+
+    :return: a boolean value to indicate whether a deletion happened.
+    """
+    try:
+        ssm_client.delete_parameter(Name=name)
+        return True
+    except Exception as e:
+        if "ParameterNotFound" in str(e):
+            return False
+        else:  # pragma: no cover
+            raise e
---
+++ tests_aws/aws/test_parameter_store.py
@@ -0,0 +1,297 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import dataclasses
+
+from pysecret.tests import bsm, py_ver, run_cov_test
+from pysecret.aws.parameter_store import (
+    ParameterTypeEnum,
+    ParameterTierEnum,
+    ParameterDataTypeEnum,
+    Parameter,
+    deploy_parameter,
+    delete_parameter,
+)
+from rich import print as rprint
+
+
+def delete_all():
+    for name in [
+        TestParameter.param_name_string,
+        TestParameter.param_name_string_list,
+        TestParameter.param_name_secure_string,
+        TestParameter.param_name_json_list,
+        TestParameter.param_name_json_dict,
+        TestParameter.param_name_object,
+    ]:
+        delete_parameter(bsm.ssm_client, name)
+
+
+def setup_module(module):
+    delete_all()
+
+
+# def teardown_module(module):
+#     delete_all()
+
+STRING = "attach on 4 AM!"
+DATA = {"name": "Alice"}
+LIST1 = [{"a": 1}, {"b": 2}]
+LIST2 = ["hello, alice", "hello, bob"]
+STRING_LIST = ["s3://my-bucket", "s3://your-bucket"]
+
+
+@dataclasses.dataclass
+class Env:
+    username: str = dataclasses.field()
+    password: str = dataclasses.field()
+
+
+@dataclasses.dataclass
+class Config:
+    dev: Env = dataclasses.field()
+    prod: Env = dataclasses.field()
+
+
+class TestParameter:
+    param_name_string = f"pysecret-{py_ver}-string"
+    param_name_string_list = f"pysecret-{py_ver}-string-list"
+    param_name_secure_string = f"pysecret-{py_ver}-secure-string"
+    param_name_json_list = f"pysecret-{py_ver}-json-list"
+    param_name_json_dict = f"pysecret-{py_ver}-json-dict"
+    param_name_object = f"pysecret-{py_ver}-object"
+
+    def test_string(self):
+        flag = delete_parameter(bsm.ssm_client, self.param_name_string)
+        assert flag is False
+
+        param = Parameter.load(bsm.ssm_client, self.param_name_string)
+        assert param is None
+
+        # use default kms key but type is not secure string
+        with pytest.raises(ValueError):
+            deploy_parameter(
+                bsm.ssm_client,
+                name=self.param_name_string,
+                data=STRING,
+                type_is_string=True,
+                tier_is_standard=True,
+                use_default_kms_key=True,
+            )
+
+        # use custom kms key but type is not secure string
+        with pytest.raises(ValueError):
+            deploy_parameter(
+                bsm.ssm_client,
+                name=self.param_name_string,
+                data=STRING,
+                type_is_string=True,
+                tier_is_standard=True,
+                kms_key_id="my-key",
+            )
+
+        # type is secure string but no kms key and not use default kms key
+        param = deploy_parameter(
+            bsm.ssm_client,
+            name=self.param_name_string,
+            data=STRING,
+            description="a string parameter",
+            type_is_secure_string=True,
+            tier_is_standard=True,
+            tags=dict(EnvName="prod"),
+        )
+        assert param.Value == STRING
+
+        param = Parameter.load(bsm.ssm_client, name=self.param_name_string)
+        assert param.Value == STRING
+
+        # test aws_account_id and aws_region
+        assert len(param.aws_account_id) == 12 and param.aws_account_id[0].isdigit()
+        assert param.aws_region == "us-east-1"
+
+    def test_string_list(self):
+        param = deploy_parameter(
+            bsm.ssm_client,
+            name=self.param_name_string_list,
+            data=STRING_LIST,
+            type_is_string_list=True,
+            tier_is_standard=True,
+        )
+        assert param.string_list == STRING_LIST
+
+        param = Parameter.load(
+            bsm.ssm_client,
+            name=self.param_name_string_list,
+        )
+        assert param.Value == ",".join(STRING_LIST)
+        assert param.string_list == STRING_LIST
+
+        # string list item doesn't support list of dict
+        with pytest.raises(TypeError):
+            deploy_parameter(
+                bsm.ssm_client,
+                name=self.param_name_string_list,
+                data=LIST1,
+                type_is_string_list=True,
+                tier_is_standard=True,
+                overwrite=True,
+            )
+
+        # string list item cannot have ","
+        with pytest.raises(ValueError):
+            deploy_parameter(
+                bsm.ssm_client,
+                name=self.param_name_string_list,
+                data=LIST2,
+                type_is_string_list=True,
+                tier_is_standard=True,
+                overwrite=True,
+            )
+
+    def test_json_list(self):
+        param = deploy_parameter(
+            bsm.ssm_client,
+            name=self.param_name_json_list,
+            data=LIST2,
+            type_is_string=True,
+            tier_is_advanced=True,
+            overwrite=True,
+        )
+        assert param.json_list == LIST2
+
+        param = Parameter.load(bsm.ssm_client, self.param_name_json_list)
+        assert param.json_list == LIST2
+
+    def test_json_dict(self):
+        param = deploy_parameter(
+            bsm.ssm_client,
+            name=self.param_name_json_dict,
+            data=DATA,
+            type_is_string=True,
+            tier_is_intelligent=True,
+            overwrite=True,
+        )
+        assert param.json_dict == DATA
+
+        param = Parameter.load(bsm.ssm_client, self.param_name_json_dict)
+        assert param.json_dict == DATA
+
+    def test_object(self):
+        config = Config(
+            dev=Env(
+                username="dev-username",
+                password="dev-password",
+            ),
+            prod=Env(
+                username="prod-username",
+                password="prod-password",
+            ),
+        )
+
+        param = deploy_parameter(
+            bsm.ssm_client,
+            name=self.param_name_object,
+            data=config,
+            type_is_string=True,
+            tier_is_intelligent=True,
+            overwrite=True,
+        )
+        py_object = param.py_object
+        assert dataclasses.asdict(py_object) == dataclasses.asdict(config)
+
+        param = Parameter.load(bsm.ssm_client, self.param_name_object)
+        assert dataclasses.asdict(param.py_object) == dataclasses.asdict(config)
+
+
+    def test_load_auto_with_encryption(self):
+        param = deploy_parameter(
+            bsm.ssm_client,
+            name=self.param_name_secure_string,
+            data={"name": "Alice"},
+            use_default_kms_key=True,
+            type_is_secure_string=True,
+            tier_is_standard=True,
+            overwrite=True,
+            skip_if_duplicated=False,
+        )
+        assert param.Type == ParameterTypeEnum.secure_string
+
+        param = Parameter.load(
+            bsm.ssm_client,
+            name=self.param_name_secure_string,
+            with_decryption=True,
+        )
+        assert param.json_dict == {"name": "Alice"}
+
+        param = Parameter.load(
+            bsm.ssm_client,
+            self.param_name_secure_string,
+        )
+        assert param.json_dict == {"name": "Alice"}
+
+    def test_deploy_parameter_skip_duplicate(self):
+        # first deployment
+        deploy_parameter(
+            bsm.ssm_client,
+            name=self.param_name_string,
+            data=DATA,
+            type_is_secure_string=True,
+            tier_is_standard=True,
+            overwrite=True,
+        )
+        param = Parameter.load(bsm.ssm_client, self.param_name_string)
+        version = param.Version # store the first deployment version
+
+        # second deployment, since the data is not changed, no deployment happen
+        deploy_parameter(
+            bsm.ssm_client,
+            name=self.param_name_string,
+            data=DATA,
+            type_is_secure_string=True,
+            tier_is_standard=True,
+            overwrite=True,
+            skip_if_duplicated=True,
+        )
+        param = Parameter.load(bsm.ssm_client, self.param_name_string)
+        assert param.Version == version
+
+        # third deployment, we set skip_if_duplicated = False, so version + 1
+        deploy_parameter(
+            bsm.ssm_client,
+            name=self.param_name_string,
+            data=DATA,
+            type_is_secure_string=True,
+            tier_is_standard=True,
+            overwrite=True,
+            skip_if_duplicated=False,
+        )
+        param = Parameter.load(bsm.ssm_client, self.param_name_string)
+        assert param.Version == version + 1
+
+        # fourth deployment, since the data is changed, so version + 1
+        deploy_parameter(
+            bsm.ssm_client,
+            name=self.param_name_string,
+            data={"key": "a new value"},
+            type_is_secure_string=True,
+            tier_is_standard=True,
+            overwrite=True,
+            skip_if_duplicated=True,
+        )
+        param = Parameter.load(bsm.ssm_client, self.param_name_string)
+        assert param.Version == version + 2
+
+    def test_invalid_args(self):
+        with pytest.raises(ValueError):
+            deploy_parameter(
+                bsm.ssm_client,
+                name=self.param_name_string,
+                data=DATA,
+                kms_key_id="my-key",
+                use_default_kms_key=True,
+                type_is_secure_string=True,
+                tier_is_standard=True,
+            )
+
+if __name__ == "__main__":
+    run_cov_test(__file__, "pysecret.aws.parameter_store", preview=False)
